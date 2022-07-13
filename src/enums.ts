fbea226bf306229adbf56ab6d92adc2be892fb14
rokas <rokasra@gmail.com>
2022-07-13 08:19:32 +0300

    no console

--- src/enums.ts
+++ src/enums.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/comma-dangle */
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable no-return-assign */
-/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
 /* eslint-disable no-lone-blocks */
